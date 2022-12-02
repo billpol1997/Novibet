6a1081ad95e71aeffa72f84288c4d9c9b36f79c8
Vasilis Polyzos <bill_polyzos@hotmail.com>
2022-12-02 13:39:37 +0200

    Initial Commit

---
+++ Novibet/NovibetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovibetApp.swift
+//  Novibet
+//
+//  Created by Bill on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NovibetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
